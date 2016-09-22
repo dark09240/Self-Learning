d54a31bf5e2a6dadb5b4e135584c6dfcd93ac4f8
Dark <dark09240@gmail.com>
2016-09-22 16:42:34 +0800

    Initial Commit

---
+++ Self-Learning/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Self-Learning
+//
+//  Created by Lycodes_Dong on 9/22/16.
+//  Copyright © 2016 Dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Self-Learning/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Self-Learning
+//
+//  Created by Lycodes_Dong on 9/22/16.
+//  Copyright © 2016 Dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
